e10733c07a4a1bfa1920adbc9dbf6930de064feb
Adam Oswalt <email@adamoswalt.com>
2017-03-08 22:38:42 -0600

    4.2.4 - iterative updates

---
+++ ch4/todo_builder.ex
@@ -0,0 +1,67 @@
+defmodule TodoList do
+  defstruct auto_id: 1, entries: HashDict.new
+
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+      #fn(entry, todo_list_acc) -> add_entry(todo_list_acc, entry) end
+    )
+  end
+
+  def add_entry(
+    %TodoList{entries: entries, auto_id: auto_id} = todo_list,
+    entry
+  ) do
+    entry = Map.put(entry, :id, auto_id)
+    new_entries = HashDict.put(entries, auto_id, entry)
+
+    %TodoList{todo_list |
+      entries: new_entries,
+      auto_id: auto_id + 1
+    }
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn(_) -> new_entry end)
+  end
+
+  def update_entry(
+    %TodoList{entries: entries} = todo_list,
+    entry_id,
+    updater_fun
+  ) do
+    case entries[entry_id] do
+      nil -> todo_list
+
+      old_entry ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entries = HashDict.put(entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(
+    %TodoList{entries: entries} = todo_list,
+    entry_id
+  ) do
+    case entries[entry_id] do
+      nil -> todo_list
+
+      _ ->
+        filtered_entries = Enum.filter(
+          entries,
+          fn({_, entry}) -> entry.id != entry_id end
+        )
+        %TodoList{todo_list | entries: filtered_entries}
+    end
+  end
+
+  def entries(%TodoList{entries: entries}, date) do
+    entries
+    |> Stream.filter(fn({_, entry}) -> entry.date == date end)
+    |> Enum.map(fn({_, entry}) -> entry end)
+  end
+end
