a651334cf3718cf758d5240ca1cdd21a31e86e94
Adam Oswalt <email@adamoswalt.com>
2017-03-12 19:01:50 -0500

    6.1.3 - using the generic abstraction

--- ch6/server_process.ex
+++ ch6/server_process.ex
@@ -29,3 +29,29 @@ defmodule ServerProcess do
     end
   end
 end
+
+defmodule KeyValueStore do
+  def start do
+    ServerProcess.start(KeyValueStore)
+  end
+
+  def put(pid, key, value) do
+    ServerProcess.call(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    ServerProcess.call(pid, {:get, key})
+  end
+
+  def init do
+    HashDict.new
+  end
+
+  def handle_call({:put, key, value}, state) do
+    {:ok, HashDict.put(state, key, value)}
+  end
+
+  def handle_call({:get, key}, state) do
+    {HashDict.get(state, key), state}
+  end
+end
